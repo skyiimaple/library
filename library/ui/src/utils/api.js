68c44ae11c0459a437525a967e226a4beb1c9997
LiYunTan <1521137703@qq.com>
2020-06-02 14:05:28 +0800

    ui-部分接口修改

--- library/ui/src/utils/api.js
+++ library/ui/src/utils/api.js
@@ -6,13 +6,12 @@ export default {
   },
   // 获取图书管理
   getBookManage (p) {
-    // return Vue.$get('/api/cms/book/list/' + p.page + '/' + p.size, { bid: p.bid })
-    return Vue.$get('/api/portal/list/book/' + p.page + '/' + p.size, { bid: p.bid })
+    return Vue.$get('/api/cms/book/list/' + p.page + '/' + p.size, { bid: p.bid })
+    // return Vue.$get('/api/portal/list/book/' + p.page + '/' + p.size, { bid: p.bid })
   },
   // 获取图书分类管理
   getCatgoryManage (p) {
     return Vue.$get('/api/cms/cat/list/', p)
-    // return Vue.$get('/api/portal/list/cat/', p)
   },
   // 获取分类标签
   getTags (p) {
